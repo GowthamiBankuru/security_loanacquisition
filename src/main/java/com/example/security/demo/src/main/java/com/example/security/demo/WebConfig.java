5e04767b853530ef347473776dbc48580ac86458
Gowthami Bankuru <166654120+GowthamiBankuru@users.noreply.github.com>
2026-07-30 12:40:08 -0400

    Add files via upload

---
+++ src/main/java/com/example/security/demo/src/main/java/com/example/security/demo/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.security.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true);
+			}
+		};
+	}
+
+}
